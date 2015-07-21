81efab663403076920e249c950c2d4600b4b84e6
peb7268 <peb7268@gmail.com>
2015-07-20 21:16:43 -0400

    initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,22 @@
+var gulp 		= require('gulp');
+var sass 		= require('gulp-sass');
+var livereload 	= require('gulp-livereload');
+
+var paths 		= {
+	sass: './public/styles/sass',
+	css:  './public/styles/css',
+	js:   './public/js'
+}
+
+gulp.task('sass', function () {
+  gulp.src(paths.sass + '/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.css));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch(paths.sass + '/**/*.scss', ['sass']);
+});
+
+
+gulp.task('default', ['sass', 'sass:watch']);
